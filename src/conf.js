ff13ed4dd573ab75beae6178f324db937d8c7191
GameYojoies <game@yojoies.com>
2024-03-20 17:31:30 +0700

    3-20

---
+++ src/conf.js
@@ -0,0 +1,21 @@
+
+//开发机
+// var APIServer='http://13.212.105.21/api/';
+// var ShareServer='https://m.slzgtest.top';
+// // var kfLink='https://www.baidu.com';
+var isPro = false;
+
+// 测试机
+var APIServer='https://api.slzgtest.top';
+var ShareServer='https://m.slzgtest.top';
+
+// 正式机
+// var APIServer='https://api.suanlicn.top';
+// var ShareServer='https://m.suanlicn.top';
+
+// var APIServer='https://m.suanlicn.top';
+// var ShareServer='https://m.suanlicn.top/m/';
+
+
+var kfLink='https://d13o86p0xrt226.cloudfront.net/1d2bae3f41c4c5875e90b7f3dc4bd746/index.html';
+// var isPro = true;
